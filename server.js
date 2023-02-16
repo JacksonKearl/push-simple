75fe16dea253d14ac667bcb40850afc17d744bdf
Jackson Kearl <jackson.kearl@outlook.com>
2023-02-16 11:23:22 -0800

    onclick

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 // Use the web-push library to hide the implementation details of the communication
 // between the application server and the push service.
 // For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol and
@@ -12,8 +14,8 @@ if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
 			'environment variables. You can use the following ones:',
 	)
 	console.log(webPush.generateVAPIDKeys())
-	return
 }
+
 // Set the keys used for encrypting the push messages.
 webPush.setVapidDetails(
 	'https://example.com/',
@@ -22,6 +24,8 @@ webPush.setVapidDetails(
 )
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/vapidPublicKey', function (req, res) {
 	res.send(process.env.VAPID_PUBLIC_KEY)
