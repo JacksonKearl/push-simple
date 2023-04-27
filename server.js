01a9f872df78361f1b05ba07cd3f3cdb205cfcd8
Jackson Kearl <jackson.kearl@outlook.com>
2023-04-27 12:32:04 -0600

    Cool

--- server.js
+++ server.js
@@ -4,58 +4,59 @@
 // between the application server and the push service.
 // For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol and
 // https://tools.ietf.org/html/draft-ietf-webpush-encryption.
-const webPush = require('web-push')
-const express = require('express')
-require('dotenv').config()
+const webPush = require("web-push")
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+
+require("dotenv").config()
 
 if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
-	console.log(
-		'You must set the VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY ' +
-			'environment variables. You can use the following ones:',
-	)
-	console.log(webPush.generateVAPIDKeys())
+  console.log(
+    "You must set the VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY " +
+      "environment variables. You can use the following ones:",
+  )
+  console.log(webPush.generateVAPIDKeys())
+  process.exit()
 }
 
 // Set the keys used for encrypting the push messages.
 webPush.setVapidDetails(
-	'https://example.com/',
-	process.env.VAPID_PUBLIC_KEY,
-	process.env.VAPID_PRIVATE_KEY,
+  "https://example.com/",
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY,
 )
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/vapidPublicKey', function (req, res) {
-	res.send(process.env.VAPID_PUBLIC_KEY)
+app.use(cors())
+
+app.get("/vapidPublicKey", function (req, res) {
+  res.send(process.env.VAPID_PUBLIC_KEY)
 })
 
-app.post('/register', function (req, res) {
-	// A real world application would store the subscription info.
-	res.sendStatus(201)
+app.post("/sendNotification", function (req, res) {
+  console.log("body", req.body)
+  const subscription = req.body.subscription
+  const payload = "" + req.body.id
+  const options = {
+    TTL: req.body.ttl,
+  }
+
+  setTimeout(function () {
+    webPush
+      .sendNotification(subscription, payload, options)
+      .then(function () {})
+      .catch(function (error) {
+        console.log(error)
+      })
+  }, req.body.delay)
+
+  res.sendStatus(200)
 })
 
-app.post('/sendNotification', function (req, res) {
-	const subscription = req.body.subscription
-	const payload = null
-	const options = {
-		TTL: req.body.ttl,
-	}
+app.use(express.static(path.join(__dirname, "public")))
 
-	setTimeout(function () {
-		webPush
-			.sendNotification(subscription, payload, options)
-			.then(function () {
-				res.sendStatus(201)
-			})
-			.catch(function (error) {
-				res.sendStatus(500)
-				console.log(error)
-			})
-	}, req.body.delay * 1000)
-})
-
-app.use(express.static(__dirname))
-
-app.listen(process.env.PORT || 4567)
+app.listen(process.env.PORT || "8080")
